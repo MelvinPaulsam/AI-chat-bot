26ec122f1872895a18f1ccab403d54347a3833cf
Melvin Paulsam <003melvin@gmail.com>
2023-07-11 10:23:28 +0530

    update one

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ import { Configuration,OpenAIApi } from 'openai'
 import readline from 'readline'
 config()
 const api=new OpenAIApi(new Configuration({
-    apiKey:process.env.API_KEY
+    apiKey:process.env.API_KEY,
 }))
 const ui=readline.createInterface({input:process.stdin,output:process.stdout})
 ui.prompt()
-api.createChatCompletion()
+ui.on('line',async input=>{
+    const res=await api.createChatCompletion({
+        model:"gpt-3.5-turbo",
+        messages:[{role:"user",content:input}],
+    })
+    console.log(res.data.choices[0].message.content)
+    ui.prompt()
+})
